2a341fe24e6a2d57647a475b791cf6e729bce3cb
Hardy Ferentschik <hardy@hibernate.org>
2016-11-17 21:52:11 +0100

    Adding mystery product

--- catalog-service/src/main/java/com/redhat/coolstore/service/CatalogService.java
+++ catalog-service/src/main/java/com/redhat/coolstore/service/CatalogService.java
@@ -19,7 +19,8 @@ public class CatalogService {
 	public CatalogService() {
 
 		products = new ArrayList<>();
-		
+
+		products.add(new Product("424242", "Mystery", "The mystery box", 10000000.00));
 		products.add(new Product("329299", "Red Fedora", "Official Red Hat Fedora", 34.99));
 		products.add(new Product("329199", "Forge Laptop Sticker", "JBoss Community Forge Project Sticker", 8.50));
 		products.add(new Product("165613", "Solid Performance Polo", "Moisture-wicking, antimicrobial 100% polyester design wicks for life of garment. No-curl, rib-knit collar; special collar band maintains crisp fold; three-button placket with dyed-to-match buttons; hemmed sleeves; even bottom with side vents; Import. Embroidery. Red Pepper.", 17.80));
--- openshift-templates/demo.sh
+++ openshift-templates/demo.sh
@@ -5,13 +5,13 @@
 # Create docker-machine and configure OpenShift via 'oc cluster up'
 docker-machine create -d virtualbox --virtualbox-cpu-count 4 --virtualbox-memory 8192 --engine-insecure-registry 172.30.0.0/16 coolstore
 eval $(docker-machine env coolstore)
-oc cluster up --metrics=true
+oc cluster up #--metrics=true
 
 # Set environment variables used throughout the script
 export OCP_PROJECT=coolstore
 export OCP_MASTER=`docker-machine ip coolstore`   # hostname or IP of the OpenShift Container Platform Master
 export OCP_DOMAIN="${OCP_MASTER}.xip.io"          # DNS domain that maps to your OpenShift Container Platform master
-export MAVEN_MIRROR_URL="http://nexus.ci.${OCP_DOMAIN}/repository/maven-public/"
+#export MAVEN_MIRROR_URL="http://nexus.ci.${OCP_DOMAIN}/repository/maven-public/"
 
 oc login -u system:admin
 
@@ -25,12 +25,12 @@ oc create -n openshift -f 'https://raw.githubusercontent.com/openshift/openshift
 oc login https://${OCP_MASTER}:8443 -p developer -u developer
 
 # Create a Maven mirror
-oc new-project ci
-oc new-app --name nexus sonatype/nexus3
-oc expose svc nexus
-oc deploy nexus --latest -n ci
-sleep 60
-oc logs -f dc/nexus
+# oc new-project ci
+# oc new-app --name nexus sonatype/nexus3
+# oc expose svc nexus
+# oc deploy nexus --latest -n ci
+# sleep 60
+# oc logs -f dc/nexus
 
 # Create the coolstore project
 oc new-project ${OCP_PROJECT}
